7c8642ac6df455954a1aa226b03eb684c65117d4
Ingolf Wagner <palipalo9@googlemail.com>
2013-05-15 19:55:05 +0200

    a bit sorting

--- src/test/scala/calendar/compare/CoreDate.scala
+++ src/test/scala/calendar/compare/CoreDate.scala
@@ -22,19 +22,30 @@ object CoreDate {
   implicit val registerOne = new DateConnect[CoreDate, One]
   implicit val registerTen = new DateConnect[CoreDate, Ten]
 
+  // -- comparison
+
   implicit val equal = new LazyCompare[CoreDate, CoreDate] {
     def equal(a: CoreDate, b: CoreDate): Boolean = a.i == b.i
 
     def less(a: CoreDate, b: CoreDate): Boolean = a.i < b.i
   }
+
+  // -- operation
+
   implicit val opOne = new DateOp[CoreDate, One] {
     def add(a: CoreDate, e: One): CoreDate = CoreDate(a.i + e.i)
   }
   implicit val opTen = new DateOp[CoreDate, Ten] {
     def add(a: CoreDate, e: Ten): CoreDate = CoreDate(a.i + 10 * e.i)
   }
+
+  // -- transfromation
+
   implicit val toRef = new DateTransformer[CoreDate, RefDate] {
     def convert(a: CoreDate): RefDate = RefDate(a.i)
+  }
+  implicit val fromRef = new DateTransformer[RefDate, CoreDate] {
+    def convert(a: RefDate): CoreDate = CoreDate(a.millis.intValue())
   }
 
 }
--- src/test/scala/calendar/compare/ShiftTenDate.scala
+++ src/test/scala/calendar/compare/ShiftTenDate.scala
@@ -10,6 +10,9 @@ import calendar.base.RefDate
 case class ShiftTenDate(i: Int)
 
 object ShiftTenDate {
+
+  // --- operation
+
   implicit val opOne = new DateOp[ShiftTenDate, One] {
     def add(a: ShiftTenDate, e: One): ShiftTenDate = ShiftTenDate(a.i + e.i)
   }
@@ -17,7 +20,7 @@ object ShiftTenDate {
     def add(a: ShiftTenDate, e: Ten): ShiftTenDate = ShiftTenDate(a.i + 10 * e.i)
   }
 
-  // --- transformation
+  // -- transformation
 
   implicit val toRef = new DateTransformer[ShiftTenDate, RefDate] {
     def convert(a: ShiftTenDate): RefDate = RefDate(10 + a.i)
