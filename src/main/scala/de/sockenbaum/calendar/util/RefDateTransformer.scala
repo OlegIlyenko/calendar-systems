1f9ffc2960d07f4ac011939110da0771236236b9
Ingolf Wagner <palipalo9@googlemail.com>
2013-05-18 21:15:02 +0200

    correct the stepper

--- src/main/scala/de/sockenbaum/calendar/util/RefDateTransformer.scala
+++ src/main/scala/de/sockenbaum/calendar/util/RefDateTransformer.scala
@@ -30,81 +30,77 @@ trait RefDateTransformer[D <: Date[D]] extends DateTransformer[RefDate, D] {
     elementList = RefDateHelper(op, element) :: elementList
   }
 
-  /**
-   * creates the fromRef date
-   * @return
-   */
-  def finished: RefDate => D = {
-    r: RefDate => {
-      /**
-       * subfunction for iteratiron
-       * @param incrementSeconds means decrement seconds
-       * @param accuSeconds seconds accumulator
-       * @param accuIterDate iterator accumulator
-       * @param accuDate date accumulator
-       * @return a date
-       */
-      @tailrec
-      def step(incrementSeconds: Boolean, accuSeconds: BigInt, accuIterDate: List[AddSubDate], accuDate: D): D = {
-        if (accuSeconds == 0) accuDate
-        else {
-          var nextIncrementSeconds = incrementSeconds
-          var nextAccuSeconds = accuSeconds
-          var nextAccuIterDate = accuIterDate
-          var nextAccuDate = accuDate
+  private def diff(a: D, b: D): BigInt = toRef.convert(a).millis - toRef.convert(b).millis
 
-          if (accuSeconds > 0) {
-            if (incrementSeconds) {
-              // switch
-              nextIncrementSeconds = false
-              nextAccuSeconds = accuSeconds
-              nextAccuIterDate = accuIterDate.tail
-              nextAccuDate = accuDate
-            } else {
-              // increment date
-              // decrement seconds
-              nextIncrementSeconds = false
-              nextAccuIterDate = accuIterDate
-              nextAccuDate = accuIterDate.head._1(accuDate)
-              // seconds here are positive
-              //nextAccuSeconds = accuSeconds - accuIterDate.head.toSecondsForAddition(accuDate)
-              nextAccuSeconds = accuSeconds - accuDate.diff(nextAccuDate)
-            }
-          } else {
-            if (incrementSeconds) {
-              // increment seconds
-              // decrement date
-              nextIncrementSeconds = true
-              nextAccuIterDate = accuIterDate
-              nextAccuDate = accuIterDate.head._2(accuDate)
-              // seconds here are negative
-              //nextAccuSeconds = accuSeconds + accuIterDate.head.toSecondsForSubtraction(accuDate)
-              nextAccuSeconds = accuSeconds + accuDate.diff(nextAccuDate)
-            } else {
-              nextIncrementSeconds = true
-              nextAccuSeconds = accuSeconds
-              nextAccuIterDate = accuIterDate.tail
-              nextAccuDate = accuDate
-            }
-          }
-          // just for debugging
-          //println((nextAccuSeconds - accuSeconds) + " : " +  incrementSeconds + "," + accuSeconds + "," + accuIterDate + "," + accuDate)
-          step(nextIncrementSeconds, nextAccuSeconds, nextAccuIterDate, nextAccuDate)
+  /**
+   * subfunction for iteratiron
+   * @param incrementSeconds means decrement seconds
+   * @param accuSeconds seconds accumulator
+   * @param accuIterElements iterator accumulator
+   * @param accuDate date accumulator
+   * @return a date
+   */
+  @tailrec
+  def step(incrementSeconds: Boolean,
+           accuSeconds: BigInt,
+           accuIterElements: List[RefDateHelper[_]],
+           accuDate: D): D = {
+    if (accuSeconds == 0) accuDate
+    else {
+      var nextIncrementSeconds = incrementSeconds
+      var nextAccuSeconds = accuSeconds
+      var nextAccuIterDate = accuIterElements
+      var nextAccuDate = accuDate
+
+      if (accuSeconds > 0) {
+        if (incrementSeconds) {
+          // switch
+          nextIncrementSeconds = false
+          nextAccuSeconds = accuSeconds
+          nextAccuIterDate = accuIterElements.tail
+          nextAccuDate = accuDate
+        } else {
+          // increment date
+          // decrement seconds
+          nextIncrementSeconds = false
+          nextAccuIterDate = accuIterElements
+          nextAccuDate = accuIterElements.head._1(accuDate)
+          // seconds here are positive
+          //nextAccuSeconds = accuSeconds - accuIterElements.head.toSecondsForAddition(accuDate)
+          nextAccuSeconds = accuSeconds - accuDate.diff(nextAccuDate)
+        }
+      } else {
+        if (incrementSeconds) {
+          // increment seconds
+          // decrement date
+          nextIncrementSeconds = true
+          nextAccuIterDate = accuIterElements
+          nextAccuDate = accuIterElements.head._2(accuDate)
+          // seconds here are negative
+          //nextAccuSeconds = accuSeconds + accuIterElements.head.toSecondsForSubtraction(accuDate)
+          nextAccuSeconds = accuSeconds + accuDate.diff(nextAccuDate)
+        } else {
+          nextIncrementSeconds = true
+          nextAccuSeconds = accuSeconds
+          nextAccuIterDate = accuIterElements.tail
+          nextAccuDate = accuDate
         }
       }
-
-      val seconds = r.millis.millis
       // just for debugging
-      // println("start " + r.seconds)
-      if (seconds == 0)
-        zeroDate
-      else if (seconds < 0)
-        step(true, r.millis.millis, iters, zeroDate)
-      else
-        step(false, r.millis.millis, iters, zeroDate)
+      //println((nextAccuSeconds - accuSeconds) + " : " +  incrementSeconds + "," + accuSeconds + "," + accuIterElements + "," + accuDate)
+      step(nextIncrementSeconds, nextAccuSeconds, nextAccuIterDate, nextAccuDate)
     }
   }
 
-  def convert(a: RefDate): D = null
+
+  def convert(r: RefDate): D = {
+    val seconds = r.millis
+    if (seconds == 0)
+      zeroDate
+    else if (seconds < 0)
+      step(true, r.millis, iters, zeroDate)
+    else
+      step(false, r.millis, iters, zeroDate)
+  }
 }
 
